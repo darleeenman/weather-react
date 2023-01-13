d73bb56374d91e2ee0c0d322163b18daab1c9d18
darleeenman <darleen@kindxd.com>
2023-01-13 15:39:37 -0800

    Update App.js
    import weather

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
-export default function Weather() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
