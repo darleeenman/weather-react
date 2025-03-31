4b7bce325ebe62cbf89628177a902b1b7c57b356
darleeenman <darleen@kindxd.com>
2025-03-30 21:08:00 -0700

    Update WeatherInfo component for Redux data structure

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,36 +1,36 @@
 import React from "react";
-import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherInfo.css";
-import WeatherTemperature from "./WeatherTemperature";
 
-export default function WeatherInfo(props) {
+export default function WeatherInfo({ data }) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
-      <div className="row mt-3">
+      <div className="row">
         <div className="col-6">
-          <div className="d-flex">
-            <div>
-              <WeatherIcon code={props.data.icon} size={52} />
-            </div>
-
-            <div>
-              <WeatherTemperature celsius={props.data.temperature} />
+          <div className="clearfix weather-temperature">
+            <img
+              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+              alt={data.weather[0].description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{Math.round(data.main.temp)}</strong>
+              <span className="units">°C</span>
             </div>
           </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Humidity: {data.main.humidity}%</li>
+            <li>Wind: {Math.round(data.wind.speed)} km/h</li>
           </ul>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <h1>{data.name}</h1>
+          <div className="weather-description">
+            {data.weather[0].description}
+          </div>
         </div>
       </div>
     </div>
